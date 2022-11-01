d791b8db1709cb78bbd1c6d536f098e8753ae4ec
João Falcão <jgabfalcao@gmail.com>
2022-11-01 10:24:11 -0300

    Busca de Produtos no mercado livre

--- busca_produtos.py
+++ busca_produtos.py
@@ -1,24 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
+lista_produtos = []
 url_base = 'https://lista.mercadolivre.com.br/'
 produto_nome = input('Qual produto você deseja? ')
 
 response = requests.get(url_base + produto_nome)
 
 site = BeautifulSoup(response.text, 'html.parser')
-#class="andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default"
 
-produto = site.find('div' , attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default'})
+produtos = site.findAll('div' , attrs={'class': 'andes-card andes-card--flat andes-card--default ui-search-result shops__cardStyles ui-search-result--core andes-card--padding-default'})
 
-titulo = produto.find('h2' , attrs={'class': 'ui-search-item__title shops__item-title'})
-titulo = (titulo.text)
+for produto in produtos:
+  titulo = produto.find('h2' , attrs={'class': 'ui-search-item__title shops__item-title'})
+  titulo = (titulo.text)
+  preco = produto.find('span' , attrs={'class': 'price-tag-amount'})
+  preco = (preco.text)
+  link = produto.find('a', attrs={'class': 'ui-search-link'})
+  link = link['href']
+  lista_produtos.append([titulo, preco, link])
+  colunas_produtos = pd.DataFrame(lista_produtos, columns=['Nome' , 'Valor', 'Link do Produto'])
 
-preco = produto.find('span' , attrs={'class': 'price-tag-amount'})
-preco = (preco.text)
 
-print(titulo)
-print(preco)
+#*! EXIBIÇÃO !*#
+print(colunas_produtos)
+
 
 
 
