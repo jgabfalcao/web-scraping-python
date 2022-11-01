f2ecd0a0d93e555f91865ad2430193d2162e5b31
João Falcão <jgabfalcao@gmail.com>
2022-11-01 11:43:51 -0300

    Acessando uma URL e pesquisando um produto com selenium

---
+++ web-scraping-selenium.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By 
+from time import sleep
+
+navegador = webdriver.Chrome()
+
+navegador.get('https://www.graodegente.com.br/')
+
+sleep(3)
+
+elemento = navegador.find_element(By.ID,'header-search')
+elemento.send_keys('144712')
+sleep(1.2)
+click_button = navegador.find_element(By.ID, 'searchSubmit')
+click_button.click()
+
+#teste teste
+
+
+
+
+
